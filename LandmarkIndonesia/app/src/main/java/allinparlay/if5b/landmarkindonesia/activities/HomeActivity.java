5614bd662c04d8f212298e9f2a5b4caf030587d6
Rizky Kurniawan <rizky.kurniawan@mhs.mdp.ac.id>
2022-12-26 20:21:07 +0700

    ngelengkapi di home, login, register

---
+++ LandmarkIndonesia/app/src/main/java/allinparlay/if5b/landmarkindonesia/activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package allinparlay.if5b.landmarkindonesia.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import allinparlay.if5b.landmarkindonesia.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
