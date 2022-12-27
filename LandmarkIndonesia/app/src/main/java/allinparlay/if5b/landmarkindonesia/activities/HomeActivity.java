8691f752d625588599b22f26ecc62264ff87a4e6
Stephen S <103354023+StephenSS1@users.noreply.github.com>
2022-12-27 20:46:26 +0700

    revisi navbar

--- LandmarkIndonesia/app/src/main/java/allinparlay/if5b/landmarkindonesia/activities/HomeActivity.java
+++ LandmarkIndonesia/app/src/main/java/allinparlay/if5b/landmarkindonesia/activities/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.FrameLayout;
 
@@ -19,29 +20,6 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        binding.navView.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()){
-
-                case R.id.navHome:
-                    replaceFragment(new HomeActivity());
-                    break;
-                case R.id.navSearch:
-                    break;
-                case R.id.navAkun:
-                    break;
-            }
-
-
-            return true;
-        });
-    }
-
-    private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame, fragment);
-        fragmentTransaction.commit();
 
 
     }
