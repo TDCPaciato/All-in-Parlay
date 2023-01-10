302c4f6a508709ae06d79582015eca791e17747c
Rizky Kurniawan <rizky.kurniawan@mhs.mdp.ac.id>
2023-01-10 12:01:55 +0700

    alhamdulillah selesai

--- Landindo/app/src/main/java/allin/if5b/landindo/Activities/AkunActivity.java
+++ Landindo/app/src/main/java/allin/if5b/landindo/Activities/AkunActivity.java
@@ -3,8 +3,10 @@ package allin.if5b.landindo.Activities;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,6 +17,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import allin.if5b.landindo.R;
+import allin.if5b.landindo.Service.Utilities;
 import allin.if5b.landindo.databinding.ActivityAboutBinding;
 import allin.if5b.landindo.databinding.ActivityAkunBinding;
 
@@ -30,7 +33,17 @@ public class AkunActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityAkunBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        binding.btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Utilities.clearData(AkunActivity.this);
+                startActivity(new Intent(AkunActivity.this, LoginActivity.class));
+                Toast.makeText(AkunActivity.this, "LOGOUT BERHASIL", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
